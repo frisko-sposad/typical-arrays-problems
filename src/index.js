a2259d37ea62997164a96809d51bf07fd884ed1c
Алексей Ренатович Сафин <60577397+frisko-sposad@users.noreply.github.com>
2020-03-16 21:50:19 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+	if (!Array.isArray(array) || array.length == 0)
   return 0;
+	array.sort(function (a, b) { return a - b;});
+	return array[0];
 }
 
 exports.max = function max (array) {
+	if (!Array.isArray(array) || array.length == 0)
   return 0;
+	array.sort(function (a, b) { return a - b;});
+	return array[array.length-1];
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+	if (!Array.isArray(array) || array.length == 0)
+	return 0;
+	
+	let sum = 0;	
+	for (let i = 0; i < array.length; i++) {
+		sum = sum + array[i];		
+	}
+	let res = sum/array.length;
+	return res;
 }
+
+
